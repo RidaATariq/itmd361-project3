f2007b1c0efc730e2e7f08963346352543255208
Miss Tariq <61063695+RidaATariq@users.noreply.github.com>
2020-11-29 16:59:29 -0700

    adding arab_cuisine.html

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// Map
+var map;
+
+function initMap() {
+  myCoordinates = {
+    lat: 21.480497,
+    lng: 39.184999
+  }
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: myCoordinates,
+    zoom: 15
+  });
+  var jeddahMarker = new google.maps.Marker({
+    position: myCoordinates,
+    map: map,
+    title: "Jeddah is known in the kingdom for its shopping districts, restaurants and cafes."
+  });
+}
+
+// To add the marker to the map, call setMap();
+jeddahMarker.setMap(map);
+window.addEventListener('load', initMap);
+
+// Slideshow
+var i = 0;
+var images = [];
+var time = 3000;
+
+images[0] = "media/bahrain-bread.jpg";
+images[1] = "media/grilled-beef-kafta-kebabs.jpg";
+images[2] = "media/labneh-with-toppings.jpg";
+images[3] = "media/qahwa-&-sweets.jpg";
+
+function changeImg() {
+  document.slide.src = images[i];
+
+  if (i < images.length - 1) {
+    i++;
+  } else {
+    i = 0;
+  }
+
+  setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg;
---
+++ js/slidescript.js
@@ -0,0 +1,30 @@
+//Automatic Slideshow
+var i = 0; // Start Point
+var images = []; // Images Array
+var time = 3000; // Time Between Switch
+
+// Image List
+images[0] = "media/bahrain-bread.jpg";
+images[1] = "media/grilled-beef-kafta-kebabs.jpg";
+images[2] = "media/labneh-with-toppings.jpg";
+images[3] = "media/qahwa-&-sweets.jpg";
+
+// Change Image
+function changeImg() {
+  document.slide.src = images[i];
+
+  // Check If Index Is Under Max
+  if (i < images.length - 1) {
+    // Add 1 to Index
+    i++;
+  } else {
+    // Reset Back To O
+    i = 0;
+  }
+
+  // Run function every x seconds
+  setTimeout("changeImg()", time);
+}
+
+// Run function when page loads
+window.onload = changeImg;
